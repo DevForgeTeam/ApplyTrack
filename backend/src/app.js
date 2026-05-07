6b176e4ef3cb160e134f32d7541e4a085616c0f4
Hüseyin Bilgiç <h.bilgic1818@gmail.com>
2026-05-07 18:47:16 +0300

    Complete initial express setup. Closes #1

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
 const app = express();
 
+// Middleware
+app.use(express.json()); 
+
+// Test route
 app.get("/", (req, res) => {
-  res.send("ApplyTrack backend is running ");
+    res.send("ApplyTrack backend is running");
 });
 
-app.listen(5001, () => {
-  console.log("Server: http://localhost:5001");
+const PORT = 5001;
+app.listen(PORT, () => {
+    console.log(`Server: http://localhost:${PORT}`);
 });
